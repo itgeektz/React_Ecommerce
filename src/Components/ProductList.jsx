b6b73773c2904af916253c27177480475ea2e57a
itgeektz <nidhin.regency@gmail.com>
2024-10-20 08:01:42 -0400

    third

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './ProductList.css'; 
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { addItemToCart } from './CartSlice';
 
 const ProductList = () => {
 
@@ -8,6 +11,12 @@ const ProductList = () => {
     { id: 2, name: 'Product B', price: 75 },
     { id: 3, name: 'Product C', price: 30 },
   ];
+  const dispatch = useDispatch();
+const [disabledProducts, setDisabledProducts] = useState([]); // State to store disabled products
+const handleAddToCart = product => {
+    dispatch(addItemToCart(product));
+    setDisabledProducts([...disabledProducts, product.id]); // Mark the product as disabled
+  };
 
   return (
     <div className="product-list">
@@ -16,6 +25,15 @@ const ProductList = () => {
         {products.map(product => (
             <li key={product.id} className='product-list-items'>
                 <span>{product.name} - Tzs- {product.price}</span>
+    <button
+    className={`add-to-cart-btn 
+    ${disabledProducts.includes(product.id) ? 'disabled' : ''}`}
+    onClick={() => handleAddToCart(product)}
+    disabled={disabledProducts.includes(product.id)} 
+    
+    >
+    Add to Cart
+</button>
                 <button>
                     Add to cart
                 </button>
